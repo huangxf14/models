baac9be530122c607c9ac13988bfa52def0e947a
Xiufeng Huang <xiufeng.huang@corp.owlii.com@NuclearC.corp.owlii.com>
2018-07-02 17:56:51 +0800

    finish gitignore

---
+++ workspace/seg/sample/run_mobilenet_v2_035_instance_3.bash
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+
+model_name="mobilenet_v2_035_instance_3"
+log_dir="dec1/train"
+
+#dataset_name="coco2017_saliency_ext"
+#tfrecord_dir="coco2017/saliency_ext/tfrecord"
+#dataset_name="owlii_studio"
+#tfrecord_dir="owlii_studio/tfrecord"
+dataset_name="coco-instance-simple"
+tfrecord_dir="coco-instance/tfrecord_simple"
+
+ckpt="mobilenet_v2_0.35_224.ckpt"
+checkpoint_exclude_scopes="MobilenetV2/Logits,MobilenetV2/Conv_1,MobilenetV2/Conv_2,MobilenetV2/Conv_3,MobilenetV2/Conv_4,MobilenetV2/Conv_5,MobilenetV2/Decoder"
+trainable_scopes="MobilenetV2/Logits,MobilenetV2/Conv_2,MobilenetV2/Conv_2,MobilenetV2/Conv_3,MobilenetV2/Conv_4,MobilenetV2/Conv_5,MobilenetV2/Decoder"
+
+num_clones_new=1
+batch_size_new=40    # 192 * 2  
+train_steps_new=10000  # 10000 steps, about 8 epoches
+second_stage_dir="all"
+num_clones=1
+batch_size=20
+train_steps=400000     # 100000 steps, about 60 epoches
+lr_decay_factor=0.87
+
+###########################################
+HOME="/home/corp.owlii.com/xiufeng.huang"
+SLIM="${HOME}/models/research/slim"
+WORKSPACE="${HOME}/models/workspace/seg"
+DATASET_DIR="${HOME}/models/workspace/seg/${tfrecord_dir}"
+INIT_CHECKPOINT="${HOME}/models/workspace/seg/model/mobilenet_v2_0.35/${ckpt}"
+TRAIN_DIR="${HOME}/models/workspace/seg/coco-instance/model_instance_reshape_3"
+#mkdir -p ${TRAIN_DIR}
+
+##### Start training #####
+# Fine-tune only the new layers
+python train_sgmt.py \
+  --train_dir=${TRAIN_DIR} \
+  --dataset_name=${dataset_name} \
+  --dataset_split_name=coco_train_instance \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=${model_name} \
+  --checkpoint_path=${INIT_CHECKPOINT} \
+  --checkpoint_exclude_scopes=${checkpoint_exclude_scopes} \
+  --trainable_scopes=${trainable_scopes} \
+  --max_number_of_steps=${train_steps_new} \
+  --batch_size=${batch_size_new} \
+  --min_scale_factor=0.5 \
+  --max_scale_factor=2.0 \
+  --scale_factor_step_size=0 \
+  --learning_rate=0.01 \
+  --learning_rate_decay_type=fixed \
+  --save_interval_secs=600 \
+  --save_summaries_secs=600 \
+  --max_ckpts_to_keep=3 \
+  --keep_ckpt_every_n_hours=1 \
+  --log_every_n_steps=10 \
+  --optimizer=rmsprop \
+  --weight_decay=0.00004 \
+  --num_clones=${num_clones_new} \
+  --use_decoder=True
+
+# Run evaluation.
+python eval_sgmt.py \
+  --checkpoint_path=${TRAIN_DIR} \
+  --eval_dir=${TRAIN_DIR} \
+  --dataset_name=${dataset_name} \
+  --dataset_split_name=coco_test_instance \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=${model_name} \
+  --batch_size=8 \
+  --min_resize_value=512 \
+  --max_resize_value=512 \
+  --use_decoder=True \
+  --max_number_of_evaluations 1  # 0 for infinite loop
+
+
+# Fine-tune all the layers
+python train_sgmt.py \
+  --train_dir=${TRAIN_DIR}/${second_stage_dir} \
+  --dataset_name=${dataset_name} \
+  --dataset_split_name=coco_train_instance \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=${model_name} \
+  --checkpoint_path=${TRAIN_DIR} \
+  --max_number_of_steps=${train_steps} \
+  --batch_size=${batch_size} \
+  --min_scale_factor=0.5 \
+  --max_scale_factor=2.0 \
+  --scale_factor_step_size=0 \
+  --learning_rate=0.01 \
+  --learning_rate_decay_type=exponential \
+  --learning_rate_decay_factor=${lr_decay_factor} \
+  --save_interval_secs=1200 \
+  --save_summaries_secs=600 \
+  --max_ckpts_to_keep=3 \
+  --keep_ckpt_every_n_hours=2.0 \
+  --log_every_n_steps=100 \
+  --optimizer=rmsprop \
+  --weight_decay=0.00004 \
+  --num_clones=${num_clones} \
+  --use_decoder=True
+
+# Run evaluation.
+python eval_sgmt.py \
+  --checkpoint_path=${TRAIN_DIR}/${second_stage_dir} \
+  --eval_dir=${TRAIN_DIR}/${second_stage_dir} \
+  --dataset_name=${dataset_name} \
+  --dataset_split_name=coco_test_instance \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=${model_name} \
+  --batch_size=8 \
+  --min_resize_value=512 \
+  --max_resize_value=512 \
+  --use_decoder=True \
+  --max_number_of_evaluations 1  # 0 for infinite loop
+
+
+
---
+++ workspace/seg/sample/val_mobilenet_v2_035_512_instance.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+log_dir="coco-instance/model_512_instance/all"
+model_name="mobilenet_v2_035_64_instance"
+
+#dataset_name="coco2017_saliency_ext"
+#tfrecord_dir="coco2017/saliency_ext/tfrecord"
+#dataset_name="owlii_studio"
+#tfrecord_dir="owlii_studio/tfrecord"
+dataset_name="coco-instance-simple"
+tfrecord_dir="coco-instance/tfrecord_simple"
+
+###################################
+HOME="/home/corp.owlii.com/xiufeng.huang"
+SLIM="${HOME}/models/research/slim"
+WORKSPACE="${HOME}/models/workspace/seg"
+DATASET_DIR="${HOME}/models/workspace/seg/${tfrecord_dir}"
+VAL_DIR="${WORKSPACE}/${log_dir}"
+
+# Run evaluation.
+python eval_sgmt.py \
+  --checkpoint_path=${VAL_DIR} \
+  --eval_dir=${VAL_DIR} \
+  --dataset_name=${dataset_name} \
+  --dataset_split_name=coco_test_instance \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=${model_name} \
+  --batch_size=1 \
+  --use_cpu=True \
+  --eval_interval_secs=10 \
+  --min_resize_value=512 \
+  --max_resize_value=512 \
+  --use_decoder=True \
+  --max_number_of_evaluations 0  # 0 for infinite loop
+
+
